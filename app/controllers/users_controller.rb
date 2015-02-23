2eb902c5180c495d7441712acfc572ab6a796e3a
RemiVR <remi.vr5@gmail.com>
2015-02-23 10:14:02 +0100

    basic user creation added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+	def index
+		@user = User.all
+	end
+	def new
+		@user = User.new
+	end
+	def show
+		@user = User.find_by(id)
+	end
+	def create
+		@user = User.new user_params
+		if @user.save
+			redirect_to users_path
+		else
+			render 'new'
+		end
+	end
+	private
+	def user_params
+		params.require(:user).permit(:name)
+	end
+end
