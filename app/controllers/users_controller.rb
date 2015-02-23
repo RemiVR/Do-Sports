c047caa7198ac771511cef176f7b880686542493
RemiVR <remi.vr5@gmail.com>
2015-02-23 11:27:44 +0100

    Display single user info and edit page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,40 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@user = User.all
-	end
-	def new
-		@user = User.new
 	end
 	def show
 		@user = User.find(params[:id])
 	end
+	def new
+		@user = User.new
+	end
 	def create
-		@user = User.new user_params
+		@user = User.new(user_params)
+
 		if @user.save
 			redirect_to users_path
 		else
 			render 'new'
 		end
 	end
+	def edit
+		
+	end
+	def update
+		 if @user.update(user_params)
+      		flash[:notice] = "Cat updated successfully"
+      		redirect_to user_path(@user)
+    	else
+      		render 'edit'
+    	end
+		
+	end
 	private
+	def set_user
+		@user = User.find(params[:id])
+	end
 	def user_params
 		params.require(:user).permit(:name, :email, :password)
 	end
