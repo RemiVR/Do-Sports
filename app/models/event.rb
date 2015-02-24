aa2946dc0381d94ea9bdc737b6c662d4681b0b8a
RemiVR <remi.vr5@gmail.com>
2015-02-24 17:42:54 +0100

    attendance asociations added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
 	belongs_to :group
-	has_many :attendences
+	has_many :users, through: :attendances
 
 	validates :title, presence: true
 	validates :date, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	belongs_to :group
-	has_many :attendences
+	has_many :events, through: :attendances
 
 	validates :name, presence: true, length: { in: 2..255 }
 	validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
